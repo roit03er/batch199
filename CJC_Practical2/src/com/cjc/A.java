b3b2735dca4b54415cd8b6c487401d8026ad1cec
Rohit Patil <rohitpatil0062@gmail.com>
2025-09-10 14:07:08 +0530

    Added add method

--- CJC_Practical2/src/com/cjc/A.java
+++ CJC_Practical2/src/com/cjc/A.java
@@ -3,8 +3,13 @@ package com.cjc;
 import java.util.Scanner;
 
 public class A {
+	public void add(){
+
+		System.out.println(10+20);
+	}
 		public static void main(String[] args) {
 			Scanner sc= new Scanner(System.in);
 			System.out.println("Rohit patil");
+			
 		}
 }	
